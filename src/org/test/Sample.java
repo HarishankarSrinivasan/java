9dc816e1932033988fcd136010b2390534a721c0
HarishankarSrinivasan <harishankar261991@gmail.com>
2021-08-03 12:07:54 +0530

    SeleProgram

---
+++ src/org/test/Sample.java
@@ -0,0 +1,23 @@
+package org.test;
+
+public class Sample {
+
+
+	private void username() {
+
+		System.out.println("Harishankar");
+
+	}
+
+	private void password() {
+
+		System.out.println("9840160108");
+	}
+    public static void main(String[] args) {
+    	Sample s =new Sample();
+    	s.username();
+    	s.password();
+		}
+}
+
+
