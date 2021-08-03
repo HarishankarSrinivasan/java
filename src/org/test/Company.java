ad2bab5626bb1df337baf2c82d9fcf736b63cec9
HarishankarSrinivasan <harishankar261991@gmail.com>
2021-08-03 11:17:15 +0530

    Javaprogram

---
+++ src/org/test/Company.java
@@ -0,0 +1,17 @@
+package org.test;
+
+public class Company {
+	
+	
+  private void username() {
+	
+	  System.out.println("Harishankar");
+
+}
+  
+  private void password() {
+
+	  System.out.println("9840160108");
+}
+
+}
